f5b9bc89f8ce4e3acff6d052c65ef2d9c4a05032
Deekshith Allamaneni <dkh.hyd@gmail.com>
2021-01-20 00:52:08 -0600

    Link auto reply text preview onClick to editor activity

---
+++ app/src/main/java/com/parishod/wareply/CustomReplyEditorActivity.java
@@ -0,0 +1,14 @@
+package com.parishod.wareply;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomReplyEditorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_reply_editor);
+    }
+}
--- app/src/main/java/com/parishod/wareply/MainActivity.java
+++ app/src/main/java/com/parishod/wareply/MainActivity.java
@@ -1,14 +1,25 @@
 package com.parishod.wareply;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    CardView autoReplyTextPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        autoReplyTextPreview = findViewById(R.id.mainAutoReplyTextCardView);
+        autoReplyTextPreview.setOnClickListener(this::openCustomReplyEditorActivity);
+    }
+
+    private void openCustomReplyEditorActivity(View v) {
+        Intent intent = new Intent(this, CustomReplyEditorActivity.class);
+        startActivity(intent);
     }
 }
