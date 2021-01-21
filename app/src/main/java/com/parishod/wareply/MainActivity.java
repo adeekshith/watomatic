fcb0ef2a4bccc72881f535263eab952dbd807d0e
Deekshith Allamaneni <dkh.hyd@gmail.com>
2021-01-20 18:52:01 -0600

    Bind custom reply data to MainActivity

--- app/src/main/java/com/parishod/wareply/MainActivity.java
+++ app/src/main/java/com/parishod/wareply/MainActivity.java
@@ -6,16 +6,27 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import com.parishod.wareply.model.CustomRepliesData;
 
 public class MainActivity extends AppCompatActivity {
-    CardView autoReplyTextPreview;
+    CardView autoReplyTextPreviewCard;
+    TextView autoReplyTextPreview;
+    CustomRepliesData customRepliesData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        autoReplyTextPreview = findViewById(R.id.mainAutoReplyTextCardView);
-        autoReplyTextPreview.setOnClickListener(this::openCustomReplyEditorActivity);
+
+        customRepliesData = CustomRepliesData.getInstance(this);
+
+        autoReplyTextPreviewCard = findViewById(R.id.mainAutoReplyTextCardView);
+        autoReplyTextPreview = findViewById(R.id.textView4);
+
+        autoReplyTextPreviewCard.setOnClickListener(this::openCustomReplyEditorActivity);
+        autoReplyTextPreview.setText(customRepliesData.get());
     }
 
     private void openCustomReplyEditorActivity(View v) {
