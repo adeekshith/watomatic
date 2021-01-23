0fe0a5f777c13783cc27e60f32a4226485f19135
SP Uday Kumar <swarnapuriuday@gmail.com>
2021-01-23 21:31:41 +0530

    Implement notification service which replies to incoming notification (#7)
    * Clicking on checkbox requests notification permission and then starts notification service which listens to incoming notifications in the background and sends reply given by the user.
    
    Co-authored-by: Deekshith Allamaneni <dkh.hyd@gmail.com>

--- app/src/main/java/com/parishod/wareply/MainActivity.java
+++ app/src/main/java/com/parishod/wareply/MainActivity.java
@@ -1,20 +1,31 @@
 package com.parishod.wareply;
 
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
 import com.parishod.wareply.model.CustomRepliesData;
 
+import static android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS;
+
 public class MainActivity extends AppCompatActivity {
+    private static final int REQ_NOTIFICATION_LISTENER = 100;
     CardView autoReplyTextPreviewCard;
     TextView autoReplyTextPreview;
     CustomRepliesData customRepliesData;
     String autoReplyTextPlaceholder;
+    Switch mainAutoReplySwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         customRepliesData = CustomRepliesData.getInstance(this);
 
+        // Assign Views
+        mainAutoReplySwitch = findViewById(R.id.mainAutoReplySwitch);
         autoReplyTextPreviewCard = findViewById(R.id.mainAutoReplyTextCardView);
         autoReplyTextPreview = findViewById(R.id.textView4);
 
@@ -30,10 +43,47 @@ public class MainActivity extends AppCompatActivity {
 
         autoReplyTextPreviewCard.setOnClickListener(this::openCustomReplyEditorActivity);
         autoReplyTextPreview.setText(customRepliesData.getOrElse(autoReplyTextPlaceholder));
+        mainAutoReplySwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked && !isListenerEnabled(MainActivity.this, NotificationService.class)){
+                launchNotificationAccessSettings();
+            }
+        });
+    }
+
+    //https://stackoverflow.com/questions/20141727/check-if-user-has-granted-notificationlistener-access-to-my-app/28160115
+    //TODO: Use in UI to verify if it needs enabling or restarting
+    public boolean isListenerEnabled(Context context, Class notificationListenerCls) {
+        ComponentName cn = new ComponentName(context, notificationListenerCls);
+        String flat = Settings.Secure.getString(context.getContentResolver(), "enabled_notification_listeners");
+        return flat != null && flat.contains(cn.flattenToString());
     }
 
     private void openCustomReplyEditorActivity(View v) {
         Intent intent = new Intent(this, CustomReplyEditorActivity.class);
         startActivity(intent);
+    }
+
+    public void launchNotificationAccessSettings() {
+        final String NOTIFICATION_LISTENER_SETTINGS;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP_MR1){
+            NOTIFICATION_LISTENER_SETTINGS = ACTION_NOTIFICATION_LISTENER_SETTINGS;
+        }else{
+            NOTIFICATION_LISTENER_SETTINGS = "android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS";
+        }
+        Intent i = new Intent(NOTIFICATION_LISTENER_SETTINGS);
+        startActivityForResult(i, REQ_NOTIFICATION_LISTENER);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQ_NOTIFICATION_LISTENER){
+            if(isListenerEnabled(this, NotificationService.class)){
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_LONG).show();
+            }else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_LONG).show();
+                mainAutoReplySwitch.setChecked(false);
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/parishod/wareply/NotificationService.java
@@ -0,0 +1,122 @@
+package com.parishod.wareply;
+
+import android.app.Notification;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.RemoteInput;
+
+import com.parishod.wareply.model.CustomRepliesData;
+
+import java.util.List;
+
+public class NotificationService extends NotificationListenerService {
+    private static final CharSequence REPLY_KEYWORD = "reply";
+    private final String TAG = NotificationService.class.getSimpleName();
+    CustomRepliesData customRepliesData;
+
+    /*
+        These are the package names of the apps. for which we want to
+        listen the notifications
+     */
+    private static final class SupportedPackageNames {
+        public static final String WHATSAPP_PACK_NAME = "com.whatsapp";
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification sbn) {
+        super.onNotificationPosted(sbn);
+        if(isSupportedPackage(sbn)) {
+            sendReply(extractWearNotification(sbn));
+        }
+    }
+
+    private void sendReply(NotificationWear notificationWear) {
+        // Possibly transient or non-user notification from WhatsApp like
+        // "Checking for new messages" or "WhatsApp web is Active"
+        if (notificationWear == null || notificationWear.remoteInputs.isEmpty()) { return;}
+
+        customRepliesData = CustomRepliesData.getInstance(this);
+
+        RemoteInput[] remoteInputs = new RemoteInput[notificationWear.remoteInputs.size()];
+
+        Intent localIntent = new Intent();
+        localIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Bundle localBundle = new Bundle();//notificationWear.bundle;
+        int i = 0;
+        for(RemoteInput remoteIn : notificationWear.remoteInputs){
+            remoteInputs[i] = remoteIn;
+            // This works. Might need additional parameter to make it for Hangouts? (notification_tag?)
+            localBundle.putCharSequence(remoteInputs[i].getResultKey(), customRepliesData.get());
+            i++;
+        }
+
+        RemoteInput.addResultsToIntent(remoteInputs, localIntent, localBundle);
+        try {
+            notificationWear.pendingIntent.send(this, 0, localIntent);
+        } catch (PendingIntent.CanceledException e) {
+            Log.e(TAG, "replyToLastNotification error: " + e.getLocalizedMessage());
+        }
+    }
+
+    //unused for now
+    private void getDetailsOfNotification(RemoteInput remoteInput) {
+        //Some more details of RemoteInput... no idea what for but maybe it will be useful at some point
+        String resultKey = remoteInput.getResultKey();
+        String label = remoteInput.getLabel().toString();
+        Boolean canFreeForm = remoteInput.getAllowFreeFormInput();
+        if(remoteInput.getChoices() != null && remoteInput.getChoices().length > 0) {
+            String[] possibleChoices = new String[remoteInput.getChoices().length];
+            for(int i = 0; i < remoteInput.getChoices().length; i++){
+                possibleChoices[i] = remoteInput.getChoices()[i].toString();
+            }
+        }
+    }
+
+    /**
+     * Extract WearNotification with RemoteInputs that can be used to send a response
+     * @param statusBarNotification
+     * @return
+     */
+    private NotificationWear extractWearNotification(StatusBarNotification statusBarNotification) {
+        //Should work for communicators such:"com.whatsapp", "com.facebook.orca", "com.google.android.talk", "jp.naver.line.android", "org.telegram.messenger"
+        NotificationWear notificationWear = new NotificationWear();
+        notificationWear.packageName = statusBarNotification.getPackageName();
+
+        NotificationCompat.WearableExtender wearableExtender = new NotificationCompat.WearableExtender(statusBarNotification.getNotification());
+        List<NotificationCompat.Action> actions = wearableExtender.getActions();
+        for(NotificationCompat.Action act : actions) {
+            if(act != null && act.getRemoteInputs() != null) {
+                for(int x = 0; x < act.getRemoteInputs().length; x++) {
+                    RemoteInput remoteInput = act.getRemoteInputs()[x];
+                    if (remoteInput.getLabel().toString().toLowerCase().contains(REPLY_KEYWORD)) {
+                        notificationWear.remoteInputs.add(remoteInput);
+                        notificationWear.pendingIntent = act.actionIntent;
+                    }
+                }
+            }
+        }
+        List<Notification> pages = wearableExtender.getPages();
+        notificationWear.pages.addAll(pages);
+
+        notificationWear.bundle = statusBarNotification.getNotification().extras;
+        notificationWear.tag = statusBarNotification.getTag();//TODO find how to pass Tag with sending PendingIntent, might fix Hangout problem
+
+        return notificationWear;
+    }
+
+    private boolean isSupportedPackage(StatusBarNotification sbn) {
+        String packageName = sbn.getPackageName();
+        switch (packageName){
+            case SupportedPackageNames.WHATSAPP_PACK_NAME:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
