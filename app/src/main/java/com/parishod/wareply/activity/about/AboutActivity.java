74f2a5726bd5e40d04b0a14b7949fb1fb9c42354
Deekshith Allamaneni <dkh.hyd@gmail.com>
2021-01-31 23:55:30 -0600

    Create AboutActivity and link it from MainActivity
    and change app name string to Watomatic

---
+++ app/src/main/java/com/parishod/wareply/activity/about/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.parishod.wareply.activity.about;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.parishod.wareply.R;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
