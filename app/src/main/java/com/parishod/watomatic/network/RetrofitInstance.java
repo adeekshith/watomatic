664a6c0d63093b363ac78bd62ff420cb8399c25f
spuday90 <swarnapuriuday@gmail.com>
2021-04-19 08:31:36 +0530

    add logging to retrofit

--- app/src/main/java/com/parishod/watomatic/network/RetrofitInstance.java
+++ app/src/main/java/com/parishod/watomatic/network/RetrofitInstance.java
@@ -1,5 +1,9 @@
 package com.parishod.watomatic.network;
 
+import com.parishod.watomatic.BuildConfig;
+
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -9,9 +13,20 @@ public class RetrofitInstance {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            if (BuildConfig.DEBUG) {
+                // set your desired log level
+                loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            }
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor) // <-- this is the important line!
+                    .build();
+
             retrofit = new retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(okHttpClient)
                     .build();
         }
         return retrofit;
