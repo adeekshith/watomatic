69c7d8da552b674bb96bf2567f1dec293ba4e502
Mawoka <mawoka-myblock@e.email>
2021-05-02 18:31:31 +0200

    Update language-de.js

--- docs/de/language-de.js
+++ docs/de/language-de.js
@@ -16,7 +16,7 @@ function set_cookie() {
     if (window.confirm("We will set a cookie if you click ok.")) {
         var myDate = new Date();
         myDate.setMonth(myDate.getMonth() + 12);
-        document.cookie = "redir=no_redir" + ";expires=" + myDate + ";domain=localhost;path=/"
+        document.cookie = "redir=no_redir" + ";expires=" + myDate + ";domain=adeekshith.github.io;path=/"
     }
 }
 
