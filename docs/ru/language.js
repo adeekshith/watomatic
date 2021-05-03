b2ce4e1661a683f35d79c3de833993f3ab0275a4
mawoka <mawoka-myblock@e.email>
2021-05-03 07:50:28 +0200

    Made cookie less anoying

--- docs/ru/language.js
+++ docs/ru/language.js
@@ -1,7 +1,29 @@
 function set_cookie() {
-    if (window.confirm("We will set a cookie if you click ok.")) {
+    var no_redir_cookie = getCookie("redir");
+    if (window.confirm("We will set a cookie if you click ok.") && no_redir_cookie == null) {
         var myDate = new Date();
         myDate.setMonth(myDate.getMonth() + 12);
-        document.cookie = "redir=no_redir" + ";expires=" + myDate + ";domain=localhost;path=/"
+        document.cookie = "redir=no_redir" + ";expires=" + myDate + ";domain=adeekshith.github.io;path=/"
     }
 }
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
