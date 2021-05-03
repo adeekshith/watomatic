b06ba1e4642b239b86705f050f55398aa7f6a8d8
mawoka <mawoka-myblock@e.email>
2021-05-03 08:11:19 +0200

    :ambulance: Cookie got the "secure" attribute, so it runs only over https!

--- docs/de/language-de.js
+++ docs/de/language-de.js
@@ -3,7 +3,7 @@ function set_cookie() {
     if (window.confirm("We will set a cookie if you click ok.") && no_redir_cookie == null) {
         var myDate = new Date();
         myDate.setMonth(myDate.getMonth() + 12);
-        document.cookie = "redir=no_redir" + ";expires=" + myDate + ";domain=adeekshith.github.io;path=/"
+        document.cookie = "redir=no_redir" + ";expires=" + myDate + ";domain=adeekshith.github.io;path=/;secure"
     }
 }
 
