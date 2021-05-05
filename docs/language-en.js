a4412455664bba6017125092cb8584cf663e3e50
mawoka <mawoka-myblock@e.email>
2021-05-05 08:15:55 +0200

    :bug: in language-en.js

--- docs/language-en.js
+++ docs/language-en.js
@@ -1,7 +1,6 @@
 function redir_lang() {
     var userLang = navigator.language || navigator.userLanguage; 
 
-    var no_redir_cookie = getCookie("redir");
     if (userLang == "de" && localStorage.getItem("redir") == null) {
         window.location.replace('/watomatic/de/index.html')
     } else if (userLang == "ru" && no_redir_cookie == null) {
