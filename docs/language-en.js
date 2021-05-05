88f882fa1889fa0e9fb8d341e0ff04d3db32c777
mawoka <mawoka-myblock@e.email>
2021-05-05 08:25:30 +0200

    Updated workflow

--- docs/language-en.js
+++ docs/language-en.js
@@ -1,6 +1,7 @@
 function redir_lang() {
     var userLang = navigator.language || navigator.userLanguage; 
 
+    
     if (userLang == "de" && localStorage.getItem("redir") == null) {
         window.location.replace('/watomatic/de/index.html')
     } else if (userLang == "ru" && no_redir_cookie == null) {
