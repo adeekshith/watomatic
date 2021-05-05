48f1674d4fe014724a0a28c495cbe39abcaaf7e9
mawoka <mawoka-myblock@e.email>
2021-05-05 08:18:08 +0200

    Removed GFonts

--- update_docsify.py
+++ update_docsify.py
@@ -4,7 +4,8 @@ import requests
 
 with open("docs/assets/vue.css", "w") as f:
     r = requests.get("https://cdn.jsdelivr.net/npm/docsify/lib/themes/vue.css")
-    f.write(r.text)
+    result = r.text.replace('@import url("https://fonts.googleapis.com/css?family=Roboto+Mono|Source+Sans+Pro:300,400,600");', "")
+    f.write(result)
 
 with open("docs/assets/emoji.min.js", "w") as f:
     r = requests.get("https://cdn.jsdelivr.net/npm/docsify/lib/plugins/emoji.min.js")
@@ -14,3 +15,5 @@ with open("docs/assets/emoji.min.js", "w") as f:
 with open("docs/assets/docsify.min.js", "w") as f:
     r = requests.get("https://cdn.jsdelivr.net/npm/docsify/lib/docsify.min.js")
     f.write(r.text)
+
+
