420449d0d3f5c3e1691d8128313ba9c073242915
spuday90 <swarnapuriuday@gmail.com>
2021-07-23 07:41:44 +0530

    replace custom apps editbox with recyclerview

--- app/src/main/java/com/parishod/watomatic/fragment/CustomAppsAdditionFragment.kt
+++ app/src/main/java/com/parishod/watomatic/fragment/CustomAppsAdditionFragment.kt
@@ -1,18 +1,11 @@
 package com.parishod.watomatic.fragment
 
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.parishod.watomatic.R
-import com.parishod.watomatic.model.logs.App
-
-import com.parishod.watomatic.model.utils.DbUtils
-import kotlinx.android.synthetic.main.fragment_custom_apps.view.*
 
 class CustomAppsAdditionFragment: Fragment()  {
     lateinit var fragmentView: View
@@ -23,60 +16,6 @@ class CustomAppsAdditionFragment: Fragment()  {
     ): View {
         fragmentView = inflater.inflate(R.layout.fragment_custom_apps, container, false)
 
-        fragmentView.customAppTextInputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                fragmentView.customAppTextInputLayout.isErrorEnabled = false
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-        })
-        fragmentView.customPackageTextInputEditText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                fragmentView.customPackageTextInputLayout.isErrorEnabled = false
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-            }
-        })
-        fragmentView.saveCustomPackageBtn.setOnClickListener{
-            when {
-                fragmentView.customAppTextInputEditText.text.isNullOrEmpty() -> {
-                    fragmentView.customAppTextInputLayout.isErrorEnabled = true
-                    fragmentView.customAppTextInputLayout.error = "App name cannot be empty"
-                }
-                fragmentView.customPackageTextInputEditText.text.isNullOrEmpty() -> {
-                    fragmentView.customPackageTextInputLayout.isErrorEnabled = true
-                    fragmentView.customPackageTextInputLayout.error = "Package name cannot be empty"
-                }
-                else -> {
-                    val dbutils = DbUtils(activity)
-                    val name = fragmentView.customAppTextInputEditText.text.toString()
-                    val packageName = fragmentView.customPackageTextInputEditText.text.toString()
-                    val app = App(
-                        name,
-                        packageName
-                    )
-                    if(dbutils.isPackageAlreadyAdded(packageName)){
-                        Toast.makeText(activity, "The package already exists, try some other app.", Toast.LENGTH_SHORT).show()
-                    }else {
-                        dbutils.insertSupportedApp(app)
-                        (fragmentView.customAppTextInputEditText.text as Editable).clear()
-                        (fragmentView.customPackageTextInputEditText.text as Editable).clear()
-                        Toast.makeText(activity, "Custom App Saved", Toast.LENGTH_SHORT).show()
-                        activity?.finish()
-                    }
-                }
-            }
-        }
         return fragmentView
     }
 }
