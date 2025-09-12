adc1311dfee0ff5f8e8c97289eca930bc9a56ca6
Deekshith Allamaneni <code@deekshith.in>
2025-09-11 21:26:14 -0500

    Add Toolbar with Back Button to Settings Activities

--- app/src/main/java/com/parishod/watomatic/activity/advancedsettings/AdvancedSettingsActivity.java
+++ app/src/main/java/com/parishod/watomatic/activity/advancedsettings/AdvancedSettingsActivity.java
@@ -1,13 +1,15 @@
 package com.parishod.watomatic.activity.advancedsettings;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-import com.parishod.watomatic.R;
-import com.parishod.watomatic.activity.BaseActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.parishod.watomatic.R;
+import com.parishod.watomatic.activity.BaseActivity;
 
 public class AdvancedSettingsActivity extends BaseActivity {
 
@@ -17,12 +19,23 @@ public class AdvancedSettingsActivity extends BaseActivity {
         WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
         setContentView(R.layout.activity_advanced_settings);
 
-        setTitle(R.string.advanced_settings);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.setting_fragment_container), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.root_layout), (v, insets) -> {
             androidx.core.graphics.Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
