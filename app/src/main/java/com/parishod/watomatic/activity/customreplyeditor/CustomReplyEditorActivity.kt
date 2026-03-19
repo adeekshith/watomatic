e1fe29b70a0a8da6d4a719bdfd3b68d9515ea582
Deekshith Allamaneni <code@deekshith.in>
2026-03-18 23:14:53 -0500

    Move refreshSubscriptionStatusIfNeeded() after initializeViews() in CustomReplyEditorActivity

--- app/src/main/java/com/parishod/watomatic/activity/customreplyeditor/CustomReplyEditorActivity.kt
+++ app/src/main/java/com/parishod/watomatic/activity/customreplyeditor/CustomReplyEditorActivity.kt
@@ -166,10 +166,9 @@ class CustomReplyEditorActivity : BaseActivity(), SharedPreferences.OnSharedPref
         val prefs = androidx.preference.PreferenceManager.getDefaultSharedPreferences(this)
         prefs.registerOnSharedPreferenceChangeListener(this)
 
-        // Refresh subscription status if needed before initializing UI
-        refreshSubscriptionStatusIfNeeded()
-
         initializeViews()
+        // Refresh subscription status after views are initialized so callbacks can update UI safely
+        refreshSubscriptionStatusIfNeeded()
         setupClickListeners()
         handleDeepLink()
 
